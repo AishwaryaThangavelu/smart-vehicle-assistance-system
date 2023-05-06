a92dbafeeca435049224a9f9260228d76aec02ae
AishwaryaThangavelu <aishwaryathangavelu@gmail.com>
2023-05-05 23:42:19 -0600

    frontend app changes

--- README.md
+++ README.md
@@ -1,23 +1,22 @@
-# Dataset
 Smart Vehicle Assistance System
-
-Research Paper:
-
-https://www.ecva.net/papers/eccv_2020/papers_ECCV/papers/123680069.pdf
+    README.md
+    jupyter-notebooks
+        smart-vehicle-assistance-system.ipynb
+        resnet_model.ipynb
+        speech_file.ipynb
+    smart-vehicle-frontend-app
+    backend-app    
 
 Datasets:
-
 https://cvrr-nas.ucsd.edu/LISA/Datasets/signDatabasePublicFramesOnly.zip
-https://cvrr-nas.ucsd.edu/LISA/lisa-traffic-sign-dataset.html
-
 https://www.kaggle.com/datasets/andrewmvd/road-sign-detection
-
 https://www.kaggle.com/datasets/ahemateja19bec1025/traffic-sign-dataset-classification
-
 https://www.kaggle.com/competitions/traffic-sign-recognition/data
-
 https://www.kaggle.com/datasets/valentynsichkar/traffic-signs-dataset-in-yolo-format
-
 https://www.kaggle.com/code/kerneler/starter-traffic-sign-data-set-ee50fb10-e/input
 
+References:
+Research Paper:
+https://www.ecva.net/papers/eccv_2020/papers_ECCV/papers/123680069.pdf
+https://cvrr-nas.ucsd.edu/LISA/lisa-traffic-sign-dataset.html
 
--- smart-vehicle-frontend-app/src/App.js
+++ smart-vehicle-frontend-app/src/App.js
@@ -2,54 +2,37 @@ import logo from './logo3.png';
 import Button from 'react-bootstrap/Button';
 import './App.css';
 import React from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 
-export async function trainAPI(data) {
-  console.log("data: ", data);
-  const response = await fetch(`/train`, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data)
-    })
-  return await response.json();
-}
-
-export async function testAPI(data) {
-  console.log("data: ", data);
-  const response = await fetch(`/test`, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data)
-    })
-  return await response.json();
-}
-
-export async function loadModelAPI(data) {
-  console.log("data: ", data);
-  const response = await fetch(`/train/load`, {
+export async function trainAPI() {
+  const response = await fetch(`http://127.0.0.1:5000/test`, {
       method: 'GET',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({user: data})
+      headers: {'Content-Type': 'application/json', 
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': "GET",
+                'Access-Control-Allow-Headers': "Content-Type"}
     })
   return await response.json();
 }
 
-export async function checkStatusAPI() {
-  console.log('here')
-  const response = await fetch(`/status`, {
+export async function testAPI() {
+  // console.log("data: ", data);
+  const response = await fetch(`http://127.0.0.1:5000/test`, {
       method: 'GET',
-      headers: {'Content-Type': 'application/json'}
-    });
-    console.log("here2", response);
+      headers: {'Content-Type': 'application/json','Access-Control-Allow-Origin': '*'}
+    })
   return await response.json();
 }
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
+    super();
     this.state = {
       trained: false,
       trainingProgress: false,
-      selectedFile: null
+      result: [],
+      prediction: "",
+      score: 0
     }
     this.startTraining = this.startTraining.bind(this);
     this.checkStatus = this.checkStatus.bind(this);
@@ -78,25 +61,24 @@ class App extends React.Component {
   startTraining() {
     console.log(this);
     this.setState({trained: true, trainingProgress: true})
-    trainAPI({}).then(response => {
+    
+    trainAPI().then(response => {
       console.log('response: ', JSON.stringify(response));
+      this.setState({trained: true, trainingProgress: false})
+      this.setState({result: response})
     });
     console.log("Click");
-  
   }
 
   test() {
-    console.log("in test");
-    testAPI(this.state.testData);
-  }
-
-  checkStatus() {
-    // }, 5000);
-    // setTimeout(() => { 
-    //   console.log("5 secs over !"); 
-    checkStatusAPI();
-    this.setState({trained: true, trainingProgress: false});
-
+    let _this = this;
+    for(let i=0; i<this.state.result.length; i++){
+      let image = this.state.result[i];
+      setTimeout(function() {
+        let img_path = image["img_path"];
+        _this.setState({selectedFile: image["img_path"], prediction: image["prediction"], score: image["score"]});
+      }, 3000 * i);
+    };
   }
 
   render() {
@@ -104,12 +86,11 @@ class App extends React.Component {
     const trainingProgress = this.state.trainingProgress;
     let p;
     if(!trained) {
-      p = <Button variant='primary' onClick={this.startTraining}>Start Training</Button>
+      p = <Button variant='primary' onClick={this.startTraining}>Train and Test Model-1</Button>
     } else if(trained && trainingProgress) {
       p = <div>
-            <h3>Training in Progress</h3>
+            <h3>Training in Progress</h3><Spinner></Spinner>
             <p class="lead">This may take some time.</p>
-            <Button variant='primary' onClick={this.checkStatus}>Check Status</Button>
           </div>
     } else {
       p = <div>
@@ -118,26 +99,11 @@ class App extends React.Component {
               Training is complete. Let us test with real world data. 
               <br/>
             </p>
+            <Button variant="primary" onClick={this.test}> Test </Button>
             <br></br>
-            <br></br>
-            <input type="file" name="file" 
-              // value={selectedFile} 
-              style={{paddingLeft:'120px'}}
-              onChange={this.onFileChange}
-            />
-            <br></br>
-            <br></br>
-            {/* <input type="file" onChange={this.onFileChange} />
-                <button onClick={this.onFileUpload}> */}
-
-            <Button variant="primary" onClick={this.onFileUpload}> Identify signal </Button>
-            <br></br>
-            <br></br>
-            {/* {this.state.selectedFile &&
-              <img alt="preview image" src={URL.createObjectURL(this.state.selectedFile)}/>
-            } */}
+            <br></br> 
             <p class="lead">
-              Detected {}_____ sign ahead. Accuracy : {}%
+              Detected <b>{this.state.prediction}</b> sign ahead. Accuracy : {this.state.score}%
             </p>
           </div>
     }
@@ -162,7 +128,8 @@ class App extends React.Component {
               <h1 class="mt-5">Smart Vehicle Assistance System</h1>
               <p class="lead">Use the Smart Vehicle Assistance System to guide on traffic signals</p>
               {this.state.trained && this.state.selectedFile ?
-              <img alt="preview image" src={URL.createObjectURL(this.state.selectedFile)}/> :
+              // <img alt="preview image" src={URL.createObjectURL(this.state.selectedFile)}/> :
+              <img alt="preview image" src={this.state.selectedFile} key={this.state.selectedFile}/> :
               <img src={logo} alt="logo" />
             }
                 <br></br>
